4e9efaed69e1aef4b4d6076aac69f2d452006e8a
Dima Bognen <bognen@gmail.com>
2018-11-22 12:43:41 -0700

    Fixed

--- login.php
+++ login.php
@@ -6,7 +6,12 @@
 <html>
 <head>
 	<title>Contact Us - Travel Experts</title>
-	<link rel="stylesheet" href="styles/styles.css">
+  <link rel="stylesheet" href="styles/styles.css">
+  <link rel="stylesheet" href="styles/loginStyles.css">
+  <!--The line below links font awesome-->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+  <!--The link below makes icons look full-->
+  <link href="bootstrap/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 	<link href="https://fonts.googleapis.com/css?family=Dancing+Script" rel="stylesheet">
 </head>
 <body>
@@ -14,15 +19,24 @@
 		include_once "header.php";
 	  include_once "menu.php";
 	?>
-  <h1>Enter your Email and Password</h1>
+  <div class="loginGroup">
+  <h3 id="loginHeader">Enter your Email and Password</h3>
   <form id="loginForm" name="loginForm" method="POST" action="verifylogin.php">
-    <div class="row">
-      <label>Email</label>
-      <input type="text" class="form-control" id="loginEmail" name="loginEmail" />
+    <div class="row loginRow">
+      <div class="col-lg-4">
+        <label>Email</label>
+      </div>
+      <div class="col-lg-8">
+        <input type="text" class="form-control" id="loginEmail" name="loginEmail" />
+      </div>
     </div>
-    <div class="row">
-      <label>Password</label>
-      <input class="form-control" type="password" id="loginPsw" name="loginPsw" />
+    <div class="row loginRow">
+      <div class="col-lg-4">
+       <label>Password</label>
+     </div>
+      <div class="col-lg-8">
+       <input class="form-control" type="password" id="loginPsw" name="loginPsw" />
+     </div>
     </div>
     <input type="submit" class="btn btn-primary btn-lg pull-right"
             id="loginSubmit" name="loginSubmit" value="Submit"/>
@@ -33,6 +47,7 @@
       $_SESSION["message"] = "";
     }
   ?>
+</div>
  <footer style="font-size:12px">Copyright &copy; 2018 by Travel Experts</footer>
 </body>
 </html>
